62ca7674a40dec2973036b1e0b4f935deb886cfb
Sandeep Krishna Donepudi <sandeep.donepudi@fortahealth.com>
2023-04-27 12:28:03 +0530

    Added Social icons & Fixed Image resolution

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -18,5 +18,7 @@ export const SOCIAL_LINKS = {
 	github: "https://github.com/StrawHatXYZ",
 	twitter: "https://twitter.com/strawhatxyz",
 	discord: "https://twitter.com/strawhatxyz",
+	medium: "https://medium.com/@StrawHatXYZ",
+	substack: "https://twitter.com/strawhatxyz",
 	email: "hello@strawhat.xyz",
 };
